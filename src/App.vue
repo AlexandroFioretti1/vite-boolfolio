7fd73ff0151d409176ac9f6f1483ff5435a70b68
AlexandroFioretti1 <alexandro.fioretti18@gmail.com>
2023-06-13 15:57:52 +0200

    add layouts for projects

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,44 @@
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from "axios";
 export default {
-    components: {
-        HelloWorld,
-    }
-}
+  components: {},
+  data() {
+    return {
+      projects: [],
+      host: "http://127.0.0.1:8000/",
+      endpoint: "api/projects",
+    };
+  },
+  methods: {},
+  mounted() {
+    const fullUrl = this.host + this.endpoint;
+    axios.get(fullUrl).then((response) => {
+      this.projects = response.data;
+    });
+  },
+};
 </script>
 
 <template>
-    <section class="vue-home">
-        <div class="container">
-            <div class="row">
-                <div class="col-12 d-flex flex-column justify-content-center align-items-center vh-100">
-                    <div class="logos">
-                        <a href="https://vitejs.dev" target="_blank">
-                            <img src="/vite.svg" class="logo" alt="Vite logo" />
-                        </a>
-                        <a href="https://vuejs.org/" target="_blank">
-                            <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-                        </a>
-                    </div>
-                    <HelloWorld />
-                </div>
-            </div>
+  <div class="container">
+    <div class="row">
+      <div class="col col-4 g-4" v-for="project in projects">
+        <div class="card">
+          <h3>Name Repository: {{ project.name }}</h3>
+          <p>Start Date: {{ project.start_date }}</p>
+          <p v-if="project.type">type: {{ project.type.name }}</p>
+          <template v-if="project.technologies">
+            <p>technologies:</p>
+            <p v-for="technology in project.technologies">
+              {{ technology.name }}
+            </p>
+          </template>
         </div>
-    </section>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style lang="scss">
-@use './styles/general.scss';
-
-.vue-home {
-    color: #2c3e50;
-    background: #181818;
-    transition: color 0.5s, background-color 0.5s;
-    line-height: 1.6;
-    font-family: Inter, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu,
-        Cantarell, 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
-    font-size: 15px;
-    text-rendering: optimizeLegibility;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-}
-
-.logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-}
-
-.logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-    filter: drop-shadow(0 0 2em #42b883aa);
-}
+@use "./styles/general.scss";
 </style>
