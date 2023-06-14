c7e26ff95c0beb22f553eee28cec182d2fa98e4e
AlexandroFioretti1 <alexandro.fioretti18@gmail.com>
2023-06-14 16:02:07 +0200

    add component for App.vue: AppHeader+ router-view

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,15 @@
 <script>
-import axios from "axios";
+import AppHeader from "./components/AppHeader.vue";
 export default {
-  components: {},
-  data() {
-    return {
-      projects: [],
-      host: "http://127.0.0.1:8000/",
-      endpoint: "api/projects",
-    };
-  },
-  methods: {},
-  mounted() {
-    const fullUrl = this.host + this.endpoint;
-    axios.get(fullUrl).then((response) => {
-      this.projects = response.data;
-    });
-  },
+  components: { AppHeader },
 };
 </script>
 
 <template>
   <div class="container">
     <div class="row">
-      <div class="col col-4 g-4" v-for="project in projects">
-        <div class="card">
-          <h3>Name Repository: {{ project.name }}</h3>
-          <p>Start Date: {{ project.start_date }}</p>
-          <p v-if="project.type">type: {{ project.type.name }}</p>
-          <template v-if="project.technologies">
-            <p>technologies:</p>
-            <p v-for="technology in project.technologies">
-              {{ technology.name }}
-            </p>
-          </template>
-        </div>
-      </div>
+      <AppHeader></AppHeader>
+      <router-view></router-view>
     </div>
   </div>
 </template>
--- src/views/AppHome.vue
+++ src/views/AppHome.vue
@@ -1,9 +1,39 @@
 <script>
-export default {};
+import axios from "axios";
+export default {
+  name: "AppHome",
+  data() {
+    return {
+      projects: [],
+      host: "http://127.0.0.1:8000/",
+      endpoint: "api/projects",
+    };
+  },
+  methods: {},
+  mounted() {
+    const fullUrl = this.host + this.endpoint;
+    axios.get(fullUrl).then((response) => {
+      this.projects = response.data;
+    });
+  },
+};
 </script>
 
 <template>
-  <div>ciao</div>
+  <div class="col col-4 g-4" v-for="project in projects">
+    <div class="card">
+      <h3>Name Repository: {{ project.name }}</h3>
+      <p>Start Date: {{ project.start_date }}</p>
+      <p v-if="project.type">type: {{ project.type.name }}</p>
+      <template v-if="project.technologies">
+        <p>technologies:</p>
+        <p v-for="technology in project.technologies">
+          {{ technology.name }}
+        </p>
+        <p>Made by: Alexandro Fioretti</p>
+      </template>
+    </div>
+  </div>
 </template>
 
 <style lang="scss" scoped></style>
